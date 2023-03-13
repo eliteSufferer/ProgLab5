0473bb9be6ac4c3f688766899c4b2bf75383f6b1
sergeyprohaker <42sdn@mail.ru>
2023-03-13 17:46:38 +0300

    Initial commit

---
+++ src/main/java/validators/CoordsValidator.java
@@ -0,0 +1,11 @@
+package validators;
+
+public class ValidateCoords {
+    public static boolean isValidX(double x){
+        return x > -201;
+    }
+
+    public static boolean isValidY(float y){
+        return y > -838;
+    }
+}
